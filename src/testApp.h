35fec16060a51741aad03a58c993b1405240c30c
Jane <friej715@newschool.edu>
2012-04-23 14:48:29 -0400

    Adding in the actual project

---
+++ src/testApp.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiPhone.h"
+#include "ofxiPhoneExtras.h"
+//#include "ofxMaxim.h"
+#include "fft.h"
+#include "ofxOsc.h"
+
+#define BUFFER_SIZE 256
+#define NUM_WINDOWS 80
+
+// for osc messages; change to whatever's needed
+#define HOST "localhost"
+#define PORT 12345
+
+class testApp : public ofxiPhoneApp{
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	
+	void touchDown(ofTouchEventArgs &touch);
+	void touchMoved(ofTouchEventArgs &touch);
+	void touchUp(ofTouchEventArgs &touch);
+	void touchDoubleTap(ofTouchEventArgs &touch);
+	void touchCancelled(ofTouchEventArgs &touch);
+
+	void audioIn( float * input, int bufferSize, int nChannels );
+
+	int		initialBufferSize;
+	int		sampleRate;
+	int		drawCounter /*, bufferCounter*/;
+	float 	* buffer;
+    
+    // sending the OSC messages
+    ofxOscSender sender;
+    
+    // volume
+    float maxLevel;
+    float maxMag;
+    float maxMagMapped;
+    
+    
+//    void audioReceived 	(float * input, int bufferSize, int nChannels); 
+    
+private:	
+    float * left;
+    float * right;
+    int 	bufferCounter;
+    fft		myfft;
+    
+    float magnitude[BUFFER_SIZE];
+    float phase[BUFFER_SIZE];
+    float power[BUFFER_SIZE];
+    
+    float freq[NUM_WINDOWS][BUFFER_SIZE/2];
+    float freq_phase[NUM_WINDOWS][BUFFER_SIZE/2];
+    
+    
+};
+
---
+++ src/testApp.mm
@@ -0,0 +1,223 @@
+#include "testApp.h"
+
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+	// IMPORTANT!!! if your sound doesn't work in the simulator - read this post - which requires you set the input stream to 24bit!!
+	//	http://www.cocos2d-iphone.org/forum/topic/4159
+
+	// register touch events
+	ofRegisterTouchEvents(this);
+	
+	ofxiPhoneSetOrientation(OFXIPHONE_ORIENTATION_LANDSCAPE_RIGHT);
+
+	ofBackground(255,255,255);
+
+	//for some reason on the iphone simulator 256 doesn't work - it comes in as 512!
+	//so we do 512 - otherwise we crash
+	initialBufferSize	= 512;
+	sampleRate 			= 44100;
+	drawCounter			= 0;
+	bufferCounter		= 0;
+	
+	buffer				= new float[initialBufferSize];
+	memset(buffer, 0, initialBufferSize * sizeof(float));
+
+	// 0 output channels,
+	// 1 input channels
+	// 44100 samples per second
+	// 512 samples per buffer
+	// 4 num buffers (latency)
+	ofSoundStreamSetup(0, 1, this, sampleRate, initialBufferSize, 4);
+	ofSetFrameRate(60);
+    
+    left = new float[BUFFER_SIZE];
+	right = new float[BUFFER_SIZE];
+    
+	for (int i = 0; i < NUM_WINDOWS; i++)
+	{
+		for (int j = 0; j < BUFFER_SIZE/2; j++)
+		{
+			freq[i][j] = 0;	
+		}
+	}
+	
+	ofSetColor(0x666666);
+    
+    // let's set up our osc sender so we can get these messages to the game
+    sender.setup(HOST, PORT);
+    
+}
+
+
+
+
+
+//--------------------------------------------------------------
+void testApp::update(){
+    // we need to keep the iphone alive--send some stuff so it doesn't turn off. the mare you gnaw!
+    if( ofGetFrameNum() % 120 == 0 ){
+		ofxOscMessage m;
+		m.setAddress( "/misc/heartbeat" );
+		m.addIntArg( ofGetFrameNum() );
+		sender.sendMessage( m );
+	}
+    
+
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    
+    maxMag = 0;
+    maxMagMapped = 0;
+    
+    ofBackground(0);
+
+	ofTranslate(0, -50, 0);
+    
+//	// draw the input:
+	ofSetHexColor(0x333333);
+	ofRect(70,100,256,200);
+	ofSetHexColor(0xFFFFFF);
+	for (int i = 0; i < initialBufferSize; i++){
+		ofLine(70+i,200,70+i,200+left[i]*100.0f);
+	}
+
+	ofSetHexColor(0x333333);
+	drawCounter++;
+	char reportString[255];
+	sprintf(reportString, "buffers received: %i\ndraw routines called: %i\n", bufferCounter,drawCounter);
+	ofDrawBitmapString(reportString, 70,308);
+
+    
+    ofSetColor(255, 255, 255,255);
+//	ofRect(600, 300, sample*150, sample*150); /* audio sigs go between -1 and 1. See?*/
+//	ofCircle(200, 300, wave*150);
+    
+    static int index=0;
+	float avg_power = 0.0f;	
+	
+	if(index < 80)
+		index += 1;
+	else
+		index = 0;
+	
+	/* do the FFT	*/
+	myfft.powerSpectrum(0,(int)BUFFER_SIZE/2, left,BUFFER_SIZE,&magnitude[0],&phase[0],&power[0],&avg_power);
+	
+	/* start from 1 because mag[0] = DC component */
+	/* and discard the upper half of the buffer */
+	for(int j=1; j < BUFFER_SIZE/2; j++) {
+		freq[index][j] = magnitude[j];	
+        // cout << freq[index][j] << endl;
+        
+        if (freq[index][j] > maxMag) {
+            maxMag = freq[index][j];
+        }
+	}
+    
+    maxMagMapped = ofMap(maxMag, 0, BUFFER_SIZE, 0, 3);
+    cout << maxMagMapped << endl;
+    
+	/* draw the FFT */
+	for (int i = 1; i < (int)(BUFFER_SIZE/2); i++){
+        if (magnitude[i] == maxMag) {
+            ofSetColor(255, 0, 0);
+        } else {
+            ofSetColor(255);
+        }
+        ofRect(maxMagMapped*100, 400, BUFFER_SIZE/3, 400-100);
+		ofLine((i*8),400,(i*8),400-magnitude[i]*10.0f);
+	}
+    
+    ofxOscMessage m;
+    m.setAddress( "/fft/levels" );
+    m.addFloatArg(maxMag);
+    sender.sendMessage(m);
+    
+    // how can we compare them? see if the equivalent freq index (?) has about the same magnitude
+}
+
+
+
+
+
+//--------------------------------------------------------------
+void testApp::audioIn(float * input, int bufferSize, int nChannels){
+			
+	if( initialBufferSize != bufferSize ){
+		ofLog(OF_LOG_ERROR, "your buffer size was set to %i - but the stream needs a buffer size of %i", initialBufferSize, bufferSize);
+		return;
+	}	
+	
+    // changes happening here!!!!!
+    // samples are "interleaved"
+    maxLevel = 0.0f;
+	for (int i = 0; i < bufferSize; i++){
+		left[i] = input[i*2];
+		right[i] = input[i*2+1];
+        
+        float abs1 = ABS(input[i*2]);
+        if (maxLevel < abs1) maxLevel = abs1;
+	}
+	bufferCounter++;
+    
+//    cout << "maxlevel" << maxLevel << endl;
+    ofxOscMessage ms;
+    ms.setAddress( "/volume/max" );
+    ms.addFloatArg(maxLevel);
+    sender.sendMessage(ms);
+	// samples are "interleaved"
+//	for (int i = 0; i < bufferSize; i++){
+//		buffer[i] = input[i];
+//	}
+//	bufferCounter++;
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchDown(ofTouchEventArgs &touch){
+    
+	ofxOscMessage m;
+//	m.setAddress( "/mouse/button" );
+//	m.addStringArg( "down" );
+//	sender.sendMessage( m );
+    
+    
+    // ok, it seems as though you can make your own "directories" as long as they match the one in the receiver.
+    // so we'll probably want 4--2 for each player, and then amplitude & frequency
+    m.setAddress( "/test/tester" );
+	m.addStringArg( "test" );
+	sender.sendMessage( m );
+}
+
+//--------------------------------------------------------------
+void testApp::touchMoved(ofTouchEventArgs &touch){
+    ofxOscMessage m;
+	m.setAddress( "/mouse/position" );
+	m.addIntArg( touch.x );
+	m.addIntArg( touch.y );
+	sender.sendMessage( m );
+}
+
+//--------------------------------------------------------------
+void testApp::touchUp(ofTouchEventArgs &touch){
+	ofxOscMessage m;
+	m.setAddress( "/mouse/button" );
+	m.addStringArg( "up" );
+	sender.sendMessage( m );
+}
+
+//--------------------------------------------------------------
+void testApp::touchDoubleTap(ofTouchEventArgs &touch){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchCancelled(ofTouchEventArgs& args){
+
+}
+
